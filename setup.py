3b9d7864b24d6f08a74cb0a53e578dd3e091b2fc
zjxmolls <zjxmolls@outlook.com>
2025-11-02 23:16:17 +0800

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
-    long_description = f.read()
+# with open('README.md') as f:
+long_description = ""
 
 
 setup(
